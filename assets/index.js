595afcebf5f8893dbed301aabc1ae141ba1f47c4
Caio Henrique Clavico Crizanto <caiohclavico@gmail.com>
2021-10-05 19:10:38 -0400

    diagonal left win condition completed

--- assets/index.js
+++ assets/index.js
@@ -4,12 +4,26 @@ let tableCol = document.getElementsByClassName('column');
 function verticalWin(){
     for (let col = 0; col < 7; col++){
         for (let row = 0; row < 3; row++){
-            if (tableCol[0].children[0].children.style.backgroundColor === tableCol[col].children[row+1].children.style.backgroundColor &&
-                tableCol[0].children[0].children.style.backgroundColor === tableCol[col].children[row+2].children.style.backgroundColor &&
-                tableCol[0].children[0].children.style.backgroundColor === tableCol[col].children[row+3].children.style.backgroundColor &&
-                tableCol[0].children[0].children.style.backgroundColor !== 'white') {
+            if (tableCol[col].children[row].children.style.backgroundColor === tableCol[col].children[row+1].children.style.backgroundColor &&
+                tableCol[col].children[row].children.style.backgroundColor === tableCol[col].children[row+2].children.style.backgroundColor &&
+                tableCol[col].children[row].children.style.backgroundColor === tableCol[col].children[row+3].children.style.backgroundColor &&
+                tableCol[col].children[row].children.style.backgroundColor !== 'white') {
                     return true;
                 };
         };
     };
 };
+
+//win condition diagonal left
+function diagonalLeftWin(){
+    for (let col = 0; col < 7; col++){
+        for (let row = 0; row < 3; row++){
+            if (tableCol[col].children[row].children.style.backgroundColor === tableCol[col-1].children[row+1].children.style.backgroundColor &&
+                tableCol[col].children[row].children.style.backgroundColor === tableCol[col-2].children[row+2].children.style.backgroundColor &&
+                tableCol[col].children[row].children.style.backgroundColor === tableCol[col-3].children[row+3].children.style.backgroundColor &&
+                tableCol[col].children[row].children.style.backgroundColor !== 'white') {
+                    return true;
+                };
+        };
+    };
+}
