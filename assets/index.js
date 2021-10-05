fa72d1acd04087f76354fbefa4ae262c7f6ead74
Caio Henrique Clavico Crizanto <caiohclavico@gmail.com>
2021-10-05 19:21:07 -0400

    diagonal right win condition completed

--- assets/index.js
+++ assets/index.js
@@ -26,4 +26,18 @@ function diagonalLeftWin(){
                 };
         };
     };
-}
+};
+
+//win condition diagonal right
+function diagonalRightWin(){
+    for (let col = 0; col < 7; col++){
+        for (let row = 0; row < 3; row++){
+            if (tableCol[col].children[row].children.style.backgroundColor === tableCol[col+1].children[row+1].children.style.backgroundColor &&
+                tableCol[col].children[row].children.style.backgroundColor === tableCol[col+2].children[row+2].children.style.backgroundColor &&
+                tableCol[col].children[row].children.style.backgroundColor === tableCol[col+3].children[row+3].children.style.backgroundColor &&
+                tableCol[col].children[row].children.style.backgroundColor !== 'white') {
+                    return true;
+                };
+        };
+    };
+};
