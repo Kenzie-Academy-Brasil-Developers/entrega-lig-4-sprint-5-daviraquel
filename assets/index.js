f4aa4b1a823d32f44d7031119a7654c3b32414c9
Caio Henrique Clavico Crizanto <caiohclavico@gmail.com>
2021-10-05 16:20:56 -0400

    4.1 finished

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,13 @@
+let tableRow = document.getElementsByClassName('cel');
+let tableCol = document.getElementsByClassName('column');
 // win condition vertical
 function verticalWin(){
     for (let col = 0; col < 7; col++){
         for (let row = 0; row < 3; row++){
-            if (disk.color[row][col] === disk.color[row+1][col] &&
-                disk.color[row][col] === disk.color[row+2][col] &&
-                disk.color[row][col] === disk.color[row+3][col] &&
-                disk.color[row][col] !== 'white') {
+            if (tableCol[0].children[0].children.style.backgroundColor === tableCol[col].children[row+1].children.style.backgroundColor &&
+                tableCol[0].children[0].children.style.backgroundColor === tableCol[col].children[row+2].children.style.backgroundColor &&
+                tableCol[0].children[0].children.style.backgroundColor === tableCol[col].children[row+3].children.style.backgroundColor &&
+                tableCol[0].children[0].children.style.backgroundColor !== 'white') {
                     return true;
                 };
         };
