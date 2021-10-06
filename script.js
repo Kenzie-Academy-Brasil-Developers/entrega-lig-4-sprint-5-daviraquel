7f83ccf149e4c9c9cfd24dcef1c69f9630193a9d
theo gandara <theogandara@gmail.com>
2021-10-06 13:08:40 -0300

    criado a condição de vitória

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-function geraArray(colunas, linhas) {
 
-    const body = document.querySelector('body')
-    const table = document.createElement('section')
-    table.setAttribute('class', 'table')
-    body.appendChild(table)
-
-    for (let i = 0; i <= colunas - 1; i++) {
-
-        const divBoard = document.createElement('div')
-        divBoard.setAttribute('class', 'column')
-        table.appendChild(divBoard)
-
-        for (let o = 0; o <= linhas; o++) {
-
-            const divCel = document.createElement('div')
-            divCel.setAttribute('class', 'cel')
-            divBoard.appendChild(divCel)
-
-        }
-    
-    }
-    return 
-}
-
-geraArray(7,6)
+function horizontalWin(){
+    for (let col = 0; col < 7; col++){
+        for (let row = 0; row < 3; row++){
+            if (tableCol[col].children[row+1].children.style.backgroundColor === tableCol[col].children[row].children.style.backgroundColor &&
+                tableCol[col].children[row+2].children.style.backgroundColor === tableCol[col].children[row].children.style.backgroundColor &&
+                tableCol[col].children[row+3].children.style.backgroundColor === tableCol[col].children[row].children.style.backgroundColor &&
+                tableCol[col].children[row].children.style.backgroundColor !== 'white') {
+                    return true;
+                };
+        };
+    };
+};
