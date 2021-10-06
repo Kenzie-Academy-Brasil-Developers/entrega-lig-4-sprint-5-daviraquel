7dcdbd1495f3afd21e44c4df76797de21a5a6dc7
Caio Henrique Clavico Crizanto <caiohclavico@gmail.com>
2021-10-06 10:10:16 -0400

    fix bug win condition

--- assets/index.js
+++ assets/index.js
@@ -3,43 +3,57 @@ let tableCol = document.getElementsByClassName('column');
 // win condition vertical
 function verticalWin(){
     for (let col = 0; col < 7; col++){
-        for (let row = 0; row < 3; row++){
-            if (tableCol[col].children[row].children.style.backgroundColor === tableCol[col].children[row+1].children.style.backgroundColor &&
-                tableCol[col].children[row].children.style.backgroundColor === tableCol[col].children[row+2].children.style.backgroundColor &&
-                tableCol[col].children[row].children.style.backgroundColor === tableCol[col].children[row+3].children.style.backgroundColor &&
-                tableCol[col].children[row].children.style.backgroundColor !== 'white') {
-                    return true;
-                };
+        for (let row = 3; row < 6; row++){
+            if (tableCol[col].children[row].lastChild !== null) {
+                if (tableCol[col].children[row].lastChild.className === tableCol[col].children[row-1].lastChild.className &&
+                    tableCol[col].children[row].lastChild.className === tableCol[col].children[row-2].lastChild.className &&
+                    tableCol[col].children[row].lastChild.className === tableCol[col].children[row-3].lastChild.className) {
+                        return true;
+                    };
+            };
         };
     };
+    return false;
 };
 
 //win condition diagonal left
 function diagonalLeftWin(){
-    for (let col = 0; col < 7; col++){
+    for (let col = 3; col < 7; col++){
         for (let row = 0; row < 3; row++){
-            if (tableCol[col].children[row].children.style.backgroundColor === tableCol[col-1].children[row+1].children.style.backgroundColor &&
-                tableCol[col].children[row].children.style.backgroundColor === tableCol[col-2].children[row+2].children.style.backgroundColor &&
-                tableCol[col].children[row].children.style.backgroundColor === tableCol[col-3].children[row+3].children.style.backgroundColor &&
-                tableCol[col].children[row].children.style.backgroundColor !== 'white') {
-                    return true;
-                };
+            if (tableCol[col].children[row].lastChild &&
+                tableCol[col-1].children[row+1].lastChild &&
+                tableCol[col-2].children[row+2].lastChild &&
+                tableCol[col-3].children[row+3].lastChild !== null) {
+
+                if (tableCol[col].children[row].lastChild.className === tableCol[col-1].children[row+1].lastChild.className &&
+                    tableCol[col].children[row].lastChild.className === tableCol[col-2].children[row+2].lastChild.className &&
+                    tableCol[col].children[row].lastChild.className === tableCol[col-3].children[row+3].lastChild.className) {
+                        return true;
+                    };
+            }
         };
     };
+    return false;
 };
 
 //win condition diagonal right
 function diagonalRightWin(){
-    for (let col = 0; col < 7; col++){
+    for (let col = 0; col < 4; col++){
         for (let row = 0; row < 3; row++){
-            if (tableCol[col].children[row].children.style.backgroundColor === tableCol[col+1].children[row+1].children.style.backgroundColor &&
-                tableCol[col].children[row].children.style.backgroundColor === tableCol[col+2].children[row+2].children.style.backgroundColor &&
-                tableCol[col].children[row].children.style.backgroundColor === tableCol[col+3].children[row+3].children.style.backgroundColor &&
-                tableCol[col].children[row].children.style.backgroundColor !== 'white') {
-                    return true;
-                };
+            if (tableCol[col].children[row].lastChild &&
+                tableCol[col+1].children[row+1].lastChild &&
+                tableCol[col+2].children[row+2].lastChild &&
+                tableCol[col+3].children[row+3].lastChild !== null) {
+
+                if (tableCol[col].children[row].lastChild.className === tableCol[col+1].children[row+1].lastChild.className &&
+                    tableCol[col].children[row].lastChild.className === tableCol[col+2].children[row+2].lastChild.className &&
+                    tableCol[col].children[row].lastChild.className === tableCol[col+3].children[row+3].lastChild.className) {
+                        return true;
+                    };
+            }
         };
     };
+    return false;
 };
 
 function geraArray(colunas, linhas) {
