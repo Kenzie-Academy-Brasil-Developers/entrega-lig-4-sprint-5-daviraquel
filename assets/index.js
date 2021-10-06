a7bb2d033c0d1b018bae88cc42bd5985a7eab3a8
zeca.rt22 <zeca.rt22@gmail.com>
2021-10-05 22:20:21 -0300

    função com condição de empate

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,5 @@
-//function by Theo
+//item 1 : function by Theo
+//gera tabela e injeta no html
 
 
 function geraArray(colunas, linhas) {
@@ -28,7 +29,9 @@ function geraArray(colunas, linhas) {
 
 geraArray(7,6)
 
-//function by Robert
+//item 2.2 : function by Robert
+//intercepta o clique na celular ta table
+//retorna o id do target
 
 document.querySelectorAll("div").forEach( function(div) {
 
@@ -40,4 +43,42 @@ document.querySelectorAll("div").forEach( function(div) {
     });
 
 });
+
+
+//item 4.4 : function by Robert
+
+
+//variável que armazena a table 
+
+let totalTableBols = document.getElementById('.table');
+
+const mSn = 'Deu empate!';
+
+
+
+
+
+//função que gera uma div com a menssagem de empate
+
+function makeMessageDraw(mSn){
+    const boxMsn = document.querySelector("#boxMsn")
+    const msn = document.createElement('div')
+    msn.innerText = mSn;
+    boxMsn.appendChild(msn);
+    return msn;
+}
+makeMessageDraw()
+
+
+
+
+//função que verifica a quantidade de elementos dentro da Table
+//caso condição satisfeita retorna div com menssagem de empate
+
+function verifyDraw(totalTableBols){
+    if (totalTableBols.childElementCount === 42){
+        return makeMessageDraw()
+    }
+}
+verifyDraw();
 
